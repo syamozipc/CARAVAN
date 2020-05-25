554cb18dc23d517a13713094903458b2809dec79
vagrant <vagrant@localhost.localdomain>
2020-05-25 02:57:13 +0000

    finish

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,21 +1,37 @@
 class BlogsController < ApplicationController
-  def index
-  end
-
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def index
-  	@blogs = Blog.all
+    # 記事を全件取得
+    @blogs = Blog.all
   end
 
   def new
-  	@blog = Blog.new
+        @blog = Blog.new
   end
+
   def create
-        blog = Blog.new(blog_params)
-        blog.save
-        redirect_to blogs_path
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blog_path(blog.id) #この行を修正
+  end
+
+  def edit
+        @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+  def destroy
+  	blog = Blog.find(params[:id])
+  	blog.destroy
+  	redirect_to blogs_path
   end
  
   private
